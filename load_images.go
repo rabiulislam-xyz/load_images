4a7bdb27b3ae85a0e46a7146dcc401900f88363f
rabiul islam <rabiulislam993@live.com>
2019-03-02 12:19:11 +0600

    initial commit

---
+++ load_images.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	file, err := os.Open("images.txt")
+	handleError(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		wg.Add(1)
+		go saveImage(scanner.Text(), &wg)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Wait()
+}
+
+func saveImage(url string, wg *sync.WaitGroup) {
+	response, err := http.Get(url)
+	handleError(err)
+	defer response.Body.Close()
+	fmt.Println("Downloaded images from ", url)
+
+	urlSlice := strings.Split(url, "/")
+	file, err := os.Create("images/" + urlSlice[len(urlSlice)-1] + ".jpg")
+	handleError(err)
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	handleError(err)
+
+	wg.Done()
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+}
